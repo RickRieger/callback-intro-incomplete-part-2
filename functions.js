26af05a1b0d11097a1b20d234de08666ac0c2f54
Rick Rieger <rickrieger36251@gmail.com>
2021-05-13 12:16:58 -0400

    initial

--- functions.js
+++ functions.js
@@ -58,29 +58,66 @@ const filter = function (collection, callback) {
 
 
 //reject([1, 2, 3, 4, 5, 6], isEven);
+// const reject = function (collection, callbackTest) {
+//   let newArray = [];
+//   each(collection, function (item){
+//     if (!callbackTest(item)){
+//       newArray.push(item);
+//     }
+//   });
+//   return newArray;
+// };
+
+
 const reject = function (collection, callbackTest) {
-  let newArray = [];
-  each(collection, function (item){
-    if (!callbackTest(item)){
-      newArray.push(item);
-    }
+  return filter(collection, function (value){
+    return !callbackTest(value);
   });
-  return newArray;
 };
+
+
+
+
+// const uniq = function (array) {
+//   let newArray = [];
+//   each(array, element =>{
+//     if (!newArray.includes(element)){
+//       newArray.push(element);
+//     }
+//   });
+//   return newArray;
+// };
 
 const uniq = function (array) {
-  let newArray = [];
-  each(array, element =>{
-    if (!newArray.includes(element)){
-      newArray.push(element);
+  let result = [];
+  each(array, function (element){
+    if (indexOf(result, element) === -1){
+      result.push(element);
     }
   });
-  return newArray;
+  return result;
 };
 
+
+// `const reduce = function(collection, iterator, accumulator) {
+//   let result = 0;
+  
+//   each(collection, function(item){
+//     if(accumulator === undefined){
+//       accumulator = item;
+//       iterator(accumulator);
+//     }else{
+//       accumulator = iterator(accumulator, item);
+//     }
+//     result = accumulator;
+//   });
+//   return result;
+// };`
+
+
 const reduce = function(collection, iterator, accumulator) {
- let result = 0;
- 
+  let result = 0;
+  
   each(collection, function(item){
     if(accumulator === undefined){
       accumulator = item;
@@ -92,6 +129,9 @@ const reduce = function(collection, iterator, accumulator) {
   });
   return result;
 };
+
+
+
 
 module.exports = {
   filter,
